ccaa2edde9b8cf3b04d52374a6f22f8f72147c01
Brian <bpcampbe@gmail.com>
2016-06-15 20:56:09 -0700

    Formatting for objectives

--- index.php
+++ index.php
@@ -8,33 +8,22 @@ $smarty->assign("Breadcrumbs", array(
 	array("Mechanical Engineering Technology", SITE_ROOT),
 	array("Objectives and Outcomes", "#")));
 $smarty->assign("Sections", array(
-	array("title" => "Objectives",
+	array("title" => "Educational Objectives",
 	      "content" => '<p>The baccalaureate degree curriculum of the Mechanical Option in Engineering Technology at New Mexico State University will prepare graduates with the knowledge base, problem solving abilities, and practical skills of mechanical aptitude necessary to enter careers that involve the design, installation, manufacture, testing, evaluation, technical sales, or maintenance of mechanical systems encountered in industry, consulting, or government. Graduates will have the ability to analyze, perform applied design, develop, implement, or oversee advanced mechanical systems and processes.</p>
-
-<p>NMSU MET (ABET Criterion 9)</p>
-
-<p>An ability to select and apply the knowledge, techniques, skills, and modern tools of the discipline; to include:</p>
- 
-<p>9a Algebra, trigonometry, Boolean mathematics, calculus, statistics and probability, fundamental principles and concepts of science and engineering technology, good practice in problem solving, and methods of standard practice in the analysis and applied design of mechanical systems.
+<h3>NMSU MET (ABET Criterion 9)</h3>
+<p><em>An ability to select and apply the knowledge, techniques, skills, and modern tools of the discipline;</em> to include:</p>
+<p><em>9a</em> Algebra, trigonometry, Boolean mathematics, calculus, statistics and probability, fundamental principles and concepts of science and engineering technology, good practice in problem solving, and methods of standard practice in the analysis and applied design of mechanical systems.
 (also ABET 3a, 3b, and 3d)</p>
- 
-<p>9b Fundamental aspects of AC and DC circuits, electrical components, computer programming logic, instrumentation principles, experimental techniques, methods of standard practice in the testing and evaluation of mechanical systems, safety consciousness, critical thinking skills, and codes and standards.
+<p><em>9b</em> Fundamental aspects of AC and DC circuits, electrical components, computer programming logic, instrumentation principles, experimental techniques, methods of standard practice in the testing and evaluation of mechanical systems, safety consciousness, critical thinking skills, and codes and standards.
 (also ABET 3a, 3b, and 3c)</p>
- 
-<p>9c Basic knowledge of manufacturing processes, engineering materials and their selection, measuring tools, machine tools, quality systems and processes, process improvement methods, economic principles, cost analysis techniques, and project management, relevant to mechanical technology areas.
+<p><em>9c</em> Basic knowledge of manufacturing processes, engineering materials and their selection, measuring tools, machine tools, quality systems and processes, process improvement methods, economic principles, cost analysis techniques, and project management, relevant to mechanical technology areas.
 (also ABET 3a, 3b, and 3d)</p>
- 
-<p>9d Current software corresponding to good practice in the application of mechanical engineering technologies. Software application functions to include: word processing, spreadsheet calculations, graphing, presentation media, computer assisted drafting and manufacturing, manufacturing processes, statistics, data acquisition, project management, and the analysis and applied design of systems involving mechanisms, machines, or fluid and thermal processes.
+<p><em>9d</em> Current software corresponding to good practice in the application of mechanical engineering technologies. Software application functions to include: word processing, spreadsheet calculations, graphing, presentation media, computer assisted drafting and manufacturing, manufacturing processes, statistics, data acquisition, project management, and the analysis and applied design of systems involving mechanisms, machines, or fluid and thermal processes.
 (also ABET 3a)</p>
- 
-<p>And, with respect to validation that graduates have been prepared for careers in their discipline, or postgraduate academic study in the discipline or a related one:</p>
-
-<p>9e Graduates will receive offers for employment positions, commensurate with the level of a Bachelor&#39;s degree in technology, or be accepted into a Master&#39;s degree program for which the degree constitutes a prerequisite.</p>
-
+ <p><em>And, with respect to validation that graduates have been prepared for careers in their discipline, or postgraduate academic study in the discipline or a related one:</em></p>
+<p><em>9e</em> Graduates will receive offers for employment positions, commensurate with the level of a Bachelor&#39;s degree in technology, or be accepted into a Master&#39;s degree program for which the degree constitutes a prerequisite.</p>
 <p>After degree completion and following 2-5 years of employment in a field-related technical area, graduates will demonstrate competence in a specialty area, or in the management of technical personnel. This is facilitated by a curriculum designed not only to provide a broad scope of technical knowledge, but also to emphasize the understanding of fundamental principles, the utilization of modern tools, and the importance of communication and team skills.</p>
-
-<p>In conjunction with the closely-related student outcomes, the above program educational objectives are intended to help ensure that graduates may better compete in a changing world.</p>
-                        '),
+<p>In conjunction with the closely-related student outcomes, the above program educational objectives are intended to help ensure that graduates may better compete in a changing world.</p>'),
 	array("title" => "Outcomes",
 	      "content" => '<strong>Graduates in the NMSU Mechanical Engineering Technology program will have:</strong>
 
