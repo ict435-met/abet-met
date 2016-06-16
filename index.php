84f953efa493d9aebeb5f2baa5e68e1d0eff6428
Brian <bpcampbe@gmail.com>
2016-06-15 21:06:09 -0700

    Formatting student outcomes (index.php)

--- index.php
+++ index.php
@@ -24,39 +24,21 @@ $smarty->assign("Sections", array(
 <p><em>9e</em> Graduates will receive offers for employment positions, commensurate with the level of a Bachelor&#39;s degree in technology, or be accepted into a Master&#39;s degree program for which the degree constitutes a prerequisite.</p>
 <p>After degree completion and following 2-5 years of employment in a field-related technical area, graduates will demonstrate competence in a specialty area, or in the management of technical personnel. This is facilitated by a curriculum designed not only to provide a broad scope of technical knowledge, but also to emphasize the understanding of fundamental principles, the utilization of modern tools, and the importance of communication and team skills.</p>
 <p>In conjunction with the closely-related student outcomes, the above program educational objectives are intended to help ensure that graduates may better compete in a changing world.</p>'),
-	array("title" => "Outcomes",
-	      "content" => '<strong>Graduates in the NMSU Mechanical Engineering Technology program will have:</strong>
-
-<p>An appropriate mastery of the knowledge, techniques, skills and modern tools of Mechanical Engineering Technology, including the following ABET Criterion 3 :</p>
-
+	array("title" => "Student Outcomes",
+	      "content" => '<h3>Graduates in the NMSU Mechanical Engineering Technology program will have:</h3>
+<p><strong>An appropriate mastery of the knowledge, techniques, skills and modern tools of Mechanical Engineering Technology, including the following ABET Criterion 3:</strong></p>
 <p>3a. an ability to select and apply the knowledge, techniques, skills, and modern tools of the discipline to broadly-defined engineering technology activities;</p>
-
 <p>3b. an ability to select and apply a knowledge of mathematics, science, engineering, and technology to engineering technology problems that require the application of principles and applied procedures or methodologies;</p>
-
 <p>3c. an ability to conduct standard tests and measurements; to conduct, analyze, and interpret experiments; and to apply experimental results to improve processes;</p>
-
 <p>3d. an ability to design systems, components, or processes for broadly-defined engineering technology problems appropriate to program educational objectives;</p>
-
 <p>3e. an ability to function effectively as a member or leader on a technical team;</p>
-
 <p>3f. an ability to identify, analyze, and solve broadly-defined engineering technology problems;</p>
-
 <p>3g. an ability to apply written, oral, and graphical communication in both technical and nontechnical environments; and an ability to identify and use appropriate technical literature;</p>
-
 <p>3h. an understanding of the need for and an ability to engage in self-directed continuing professional development;</p>
-
 <p>3i. an understanding of and a commitment to address professional and ethical responsibilities including a respect for diversity;</p>
-
-<p>3j. a knowledge of the impact of engineering technology solutions in a societal and global
-
-context; and</p>
-
+<p>3j. a knowledge of the impact of engineering technology solutions in a societal and global context; and</p>
 <p>3k. a commitment to quality, timeliness, and continuous improvement.</p>
-
- 
-<p>All student outcomes are listed and mapped under Criterion 3.</p>
-                        ')
+<p><strong>All student outcomes are listed and mapped under Criterion 3.</strong></p>')
 ));
 $smarty->display('index.tpl');
-
 ?>
