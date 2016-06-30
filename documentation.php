aad558106c366701113696fd530067bee24209ad
aaron-j-wood <aaronjwood@outlook.com>
2016-06-29 18:21:48 -0600

    Adding a new page to documentation maybe

--- documentation.php
+++ documentation.php
@@ -80,10 +80,13 @@ if ($pageUrl[1] == "courses") {
 		      "content" => '<p>What is Outcomes measures? Who knows?</p>'),
 		array("title" => 'Objectives and Outcomes Archives',
                       "titleLink" => $url."objectives-and-outcomes-archives/",
-		      "content" => '<p>What is this? Who knows?</p>'),
+		      "content" => '<p>What is this? Who knows?</p>'
+			  		  "titleLink" => $url."test/",
+			   "content" => '<p>test</p>'),
 		array("title" => 'IAC Agenda and Minutes',
                       "titleLink" => $url."iac-agenda-and-minutes/",
-		      "content" => '<p>What is this? Who knows?</p>'),
+		      "content" => '<p>What is this? Who knows?</p>'
+					),
 		array("title" => 'Options Assessments Meetings',
                       "titleLink" => $url."options-assessments-meetings/",
 		      "content" => '<p>What is this? Who knows?</p>'),
