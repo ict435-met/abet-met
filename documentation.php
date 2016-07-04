b69e1fc0d9a3479e7d3a085259555494d119ef51
Brian <bpcampbe@gmail.com>
2016-07-03 21:10:35 -0700

    Add filters for all future documentation sub-pages. Clean up some debug things

--- documentation.php
+++ documentation.php
@@ -10,37 +10,41 @@ $pageTemplate = 'two-column.tpl';
 // Process that page data we're 'hopefully' given
 $pageUrl = explode('/', strtolower($_GET['page']));
 
-/* If we're given a link of courses/foo/bar 
- */
 if ($pageUrl[1] == "courses") {
 	@include_once("data/courses.data");
 } else if ($pageUrl[1] == "objectives-and-outcomes-archives") {
-		$pageTemplate = 'one-column.tpl';
-		include("data/RockObjects.data");
+	@include_once("data/RockObjects.data");
+//} else if ($pageUrl[1] == "outcome-measures") {
+//	@include_once("data/outcome_measures.data");
+//} else if ($pageUrl[1] == "iac-agenda-and-minutes") {
+//	@include_once("data/iac.data");
+//} else if ($pageUrl[1] == "options-assessments-meetings") {
+//	@include_once("data/ioptions_assessments.data");
+
+/* If there is no match for the sub-page display the default documentation portal */
 } else {
 	$pageTemplate = 'one-column.tpl';
-	$smarty->assign("ContentTitle","Documentation");
-	$smarty->assign("ContentSubtitle",$_GET['page']);
+	$smarty->assign("ContentTitle", "Documentation");
+	$smarty->assign("ContentSubtitle", "Mechanical Engineering Technology");
 	$url = SITE_ROOT . "documentation/";
 	$sections = array(
 		array("title" => 'Outcome Measures (WEAVE Documentation)',
                       "titleLink" => $url."outcome-measures/",
-		      "content" => '<p>What is Outcomes measures? Who knows?</p>'),
+		      "content" => '<!-- -->'),
 		array("title" => 'Objectives and Outcomes Archives',
                       "titleLink" => $url."objectives-and-outcomes-archives/",
-		      "content" => '<p>What is this? Who knows?</p>'),
+		      "content" => '<!-- -->'),
 		array("title" => 'IAC Agenda and Minutes',
                       "titleLink" => $url."iac-agenda-and-minutes/",
-		      "content" => '<p>What is this? Who knows?</p>'),
+		      "content" => '<!-- -->'),
 		array("title" => 'Options Assessments Meetings',
                       "titleLink" => $url."options-assessments-meetings/",
-		      "content" => '<p>What is this? Who knows?</p>'),
+		      "content" => '<!-- -->'),
 		array("title" => 'Courses',
                       "titleLink" => $url."courses/",
-		      "content" => '<p>What is this? Who knows?</p>'),
+		      "content" => '<!-- -->'),
 	);
 } 
-		
 
 $smarty->assign("Sections", $sections);
 $smarty->assign("Breadcrumbs", $bread);
